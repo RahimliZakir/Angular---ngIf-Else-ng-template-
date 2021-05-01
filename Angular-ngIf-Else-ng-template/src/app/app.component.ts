2b4ba956cacf4d0123f758f7116bced013c52689
Zakir Rahimli <zakirer@code.edu.az>
2021-05-01 14:11:34 +0400

    TheEnd

---
+++ Angular-ngIf-Else-ng-template/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Angular-ngIf-Else-ng-template';
+
+  arr: number[] = [/*1, 2, 3, 4, 5, 6, 7, 8, 9, 10*/];
+}
